b4630c921bba1515536d6303f241fb9a40763fa0
Elmaroufi <fxxracine174@gmail.com>
2022-04-07 18:02:28 +0200

    first page

--- app/src/main/java/com/example/myapplication/HomePageActivity.java
+++ app/src/main/java/com/example/myapplication/HomePageActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.myapplication.Fragments.BlankFragment;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Fragment fm = new BlankFragment();
+        setFragment(fm);
+
+    }
+    public void setFragment(Fragment fragment) {
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.flFragment, fragment);
+        t.commit();
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.myapplication.Fragments.BlankFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
